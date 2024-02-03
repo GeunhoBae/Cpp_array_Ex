53d14dd5dc658fcd785deeb4b1b1351a604f78eb
GeunhoBae <105783525+GeunhoBae@users.noreply.github.com>
2024-02-03 19:05:53 +0900

    Add files via upload

---
+++ GameCoding/GameCoding/GameCoding.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+#include "Helper.h"
+#include "Map.h"
+#include "Player.h"
+#include "windows.h"
+
+int main()
+{
+	SetCursorOnOff(false);
+
+	while (true)
+	{
+		//입력
+		HandleKeyInput();
+
+		//로직
+		HandleMove();
+
+		//출력
+		PrintMap2D();
+	}
+}
